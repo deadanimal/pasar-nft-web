bb3087c3af904bb351ee0fd40c3f10d9b18b17c2
Saiful Azhar <saiful@Saifuls-MacBook-Air.local>
2022-03-28 12:53:32 +0800

    Compile for deploy

--- vue.config.js
+++ vue.config.js
@@ -13,10 +13,5 @@ module.exports = defineConfig({
     plugins: [
       new NodePolyfillPlugin()
     ]
-  },
-  devServer: {
-    host: 'pasar-nft-web.onrender.com',
-    https: true,
-    port:80
   }
 })
