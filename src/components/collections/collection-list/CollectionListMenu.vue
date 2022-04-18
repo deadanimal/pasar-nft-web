88cb77aaa629008aa4ca38cd1bdd56751d2f510b
Saiful Azhar <saiful@Saifuls-MacBook-Air.local>
2022-04-18 10:42:48 +0800

    Refactor collection list menu

---
+++ src/components/collections/collection-list/CollectionListMenu.vue
@@ -0,0 +1,114 @@
+<template>
+	<div class="mb-4 border-b breakout overflow-x-scroll lg:overflow-x-hidden px-4 lg:flex lg:justify-center pb-4" data-component="components.collections.collection-list.CollectionListMenu">
+        <ul class="flex justify-between lg:px-4" style="min-width: 1024px;">
+            <li class="pb-4 border-b-4 border-sky-600">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Trending</span>
+                </a>
+            </li>
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Top</span>
+                </a>
+            </li>
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Art</span>
+                </a>
+            </li>
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Collectibles</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Domain Names</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Music</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Photography</span>
+                </a>
+            </li>
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Sports</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Trading Cards</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Utility</span>
+                </a>
+            </li>
+
+            <li class="pb-4">
+                <a                    
+                    class=""
+                    href=""
+                >
+                    <span class="font-semibold text-gray-500">Virtual Worlds</span>
+                </a>
+            </li>
+
+            
+        </ul>            
+    </div>
+</template>
+
+<script>
+	
+
+
+	export default {
+
+		name: 'CollectionListMenu'
+
+	}
+
+</script>
--- src/views/collections/CollectionList.vue
+++ src/views/collections/CollectionList.vue
@@ -23,107 +23,8 @@
 
         </div>
 
-        <div class="mb-4 border-b breakout overflow-x-scroll lg:overflow-x-hidden px-4 lg:flex lg:justify-center pb-4">
-            <ul class="flex justify-between lg:px-4" style="min-width: 1024px;">
-                <li class="pb-4 border-b-4 border-sky-600">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Trending</span>
-                    </a>
-                </li>
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Top</span>
-                    </a>
-                </li>
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Art</span>
-                    </a>
-                </li>
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Collectibles</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Domain Names</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Music</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Photography</span>
-                    </a>
-                </li>
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Sports</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Trading Cards</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Utility</span>
-                    </a>
-                </li>
-
-                <li class="pb-4">
-                    <a                    
-                        class=""
-                        href=""
-                    >
-                        <span class="font-semibold text-gray-500">Virtual Worlds</span>
-                    </a>
-                </li>
-
-                
-            </ul>            
-        </div>
-
+        
+        <collection-list-menu></collection-list-menu>
 
 
 
@@ -173,6 +74,8 @@
     import { useStore } from 'vuex';
     import { computed } from 'vue';
 
+    import CollectionListMenu from '@/components/collections/collection-list/CollectionListMenu.vue'
+
 /*
  ######    ##            ###      ######     ######    
 ##    ##   ##           ## ##    ##    ##   ##    ##   
@@ -182,9 +85,15 @@
 ##    ##   ##         ##     ##  ##    ##   ##    ##   
  ######    ########   ##     ##   ######     ######    
 */
+
+
     export default {
 
         name: 'CollectionList',
+
+        components: {
+            CollectionListMenu
+        },
 
         setup(){
             // data
